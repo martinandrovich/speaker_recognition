a3a77317537722f81694e08b9cddf9c12e58e4c6
Martin Androvich <martinandrovich@gmail.com>
2022-01-25 15:57:41 +0100

    update dataset

--- datasets/PodcastAudioDataset/PodcastAudioDataset.py
+++ datasets/PodcastAudioDataset/PodcastAudioDataset.py
@@ -9,8 +9,6 @@ from math import ceil
 from torch.utils.data import Dataset
 from itertools import chain
 
-import sounddevice as sd
-
 class PodcastAudioDataset(Dataset):
 
 	# https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
@@ -31,20 +29,24 @@ class PodcastAudioDataset(Dataset):
 			l = self.files
 			l = list(chain.from_iterable(zip(l[:len(l)//2], l[len(l)//2:]))) # alternate list shuffle (0,1,0,1,...)
 			self.files = l[int(len(l) * range[0]) : int(len(l) * range[1])] # splice by percent given by 'range'
-
+		
+		# extra transforms
 		self.transform = transform
 		self.target_transform = target_transform
 
 		# optional arguments (from **kwargs)
 		self.sample_rate = 16_000 # desired sample rate
 		self.num_divs = kwargs["num_divs"] if "num_divs" in kwargs else 1 # number of sub-samples
-		self.mel_spectrogram = False
-		self.augments = kwargs["augments"] if "augments" in kwargs else [] # "time_shift", "spec_augment"
+		self.mel_spec = kwargs["mel_spec"] if "mel_spec" in kwargs else False # use MelSpectrogram
+		self.augments = kwargs["augments"] if "augments" in kwargs else [] # ["time_shift", "spec_augment"]
 
 		# set dataset length (based on number of divisions)
 		self.len = len(self.files) * self.num_divs
 
-		print("Loaded PodcastAudioDataset from: ", self.dir_data)
+		print(f"Loaded PodcastAudioDataset ({split}) from: {self.dir_data}")
+		
+	def sample_size(self):
+		return self[0][0].size()
 
 	def __len__(self):
 		return self.len
@@ -62,6 +64,8 @@ class PodcastAudioDataset(Dataset):
 		file = self.files[idx_file][0]; label = self.files[idx_file][1]
 		waveform, sample_rate = torchaudio.load(file)
 		dur = waveform.size()[1] / sample_rate * 1000
+		
+		# print(file, self.sample_rate, label)
 
 		# -------------------------------------------------------------------------
 
@@ -80,11 +84,6 @@ class PodcastAudioDataset(Dataset):
 			chunks = torch.chunk(waveform.T, self.num_divs)
 			waveform = chunks[idx_sample].T
 
-		# debug
-		print(file, self.sample_rate, label)
-		sd.play(waveform.T, self.sample_rate)
-		sd.wait()
-
 		# -------------------------------------------------------------------------
 
 		# augmentation (time domain)
@@ -96,17 +95,19 @@ class PodcastAudioDataset(Dataset):
 
 		# mel spectrogram
 
-		if self.mel_spectrogram:
+		if self.mel_spec:
 
-			spectrogram = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(waveform)
-			spectrogram = transforms.AmplitudeToDB(top_db=top_db)(spectrogram)
+			spec = torchaudio.transforms.MelSpectrogram(self.sample_rate, n_fft=1024, hop_length=None, n_mels=64)(waveform)
+			spec = torchaudio.transforms.AmplitudeToDB(top_db=80)(spec)
+			
+			# augmentation (frequency domain)
 
 			if "spec_augment" in self.augments:
-				pass
+				spec = spec_augment(spec)
 
 		# -------------------------------------------------------------------------
 
-		# apply transforms
+		# apply extra transforms
 
 		if self.transform:
 			data = self.transform(data)
@@ -116,7 +117,7 @@ class PodcastAudioDataset(Dataset):
 
 		# -------------------------------------------------------------------------
 
-		return spectrogram if self.mel_spectrogram else waveform, label
+		return (spec if self.mel_spec else waveform, label)
 
 def resize_waveform(waveform, max_ms, sample_rate):
 
@@ -126,30 +127,34 @@ def resize_waveform(waveform, max_ms, sample_rate):
 	max_len = sample_rate//1000 * max_ms
 
 	if (sig_len > max_len):
-		# Truncate the signal to the given length
+		# truncate the signal to the given length
 		waveform = waveform[:,:max_len]
 
 	elif (sig_len < max_len):
-		# Length of padding to add at the beginning and end of the signal
+		# length of padding to add at the beginning and end of the signal
 		pad_begin_len = random.randint(0, max_len - sig_len)
 		pad_end_len = max_len - sig_len - pad_begin_len
 
-		# Pad with 0s
+		# pad with 0s
 		pad_begin = torch.zeros((num_rows, pad_begin_len))
 		pad_end = torch.zeros((num_rows, pad_end_len))
 
 		waveform = torch.cat((pad_begin, waveform, pad_end), 1)
 
 	return waveform
+	
+def spec_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
 
-	def spectro_gram(aud, n_mels=64, n_fft=1024, hop_len=None):
+	_, n_mels, n_steps = spec.shape
+	mask_value = spec.mean()
+	spec_aug = spec
 
-		sig,sr = aud
-		top_db = 80
+	freq_mask_param = max_mask_pct * n_mels
+	for _ in range(n_freq_masks):
+	  spec_aug = torchaudio.transforms.FrequencyMasking(freq_mask_param)(spec_aug, mask_value)
 
-		# spec has shape [channel, n_mels, time], where channel is mono, stereo etc
-		spec = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(sig)
+	time_mask_param = max_mask_pct * n_steps
+	for _ in range(n_time_masks):
+	  spec_aug = torchaudio.transforms.TimeMasking(time_mask_param)(spec_aug, mask_value)
 
-		# Convert to decibels
-		spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
-		return (spec)
+	return spec_aug
